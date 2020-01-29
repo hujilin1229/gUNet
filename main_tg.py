dd032d73daf6efde96014192191fd8d93ada9e2b
Jilin Hu <hujilin1229@163.com>
2020-01-29 17:59:55 +0400

    add tg implementation

--- main_tg.py
+++ main_tg.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 import math
-from network import GUNet
+from network_tg import GUNet
 from mlp_dropout import MLPClassifier
 from sklearn import metrics
 from util import cmd_args, load_data
@@ -22,13 +22,13 @@ class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
         model = GUNet
-
         self.s2v = model(
             latent_dim=cmd_args.latent_dim,
             output_dim=cmd_args.out_dim,
-            num_node_feats=cmd_args.feat_dim+cmd_args.attr_dim,
+            num_node_feats=cmd_args.feat_dim + cmd_args.attr_dim,
             num_edge_feats=0,
             k=cmd_args.sortpooling_k)
+
         out_dim = cmd_args.out_dim
         if out_dim == 0:
             out_dim = self.s2v.dense_dim
@@ -87,8 +87,67 @@ class Classifier(nn.Module):
 
         return node_feat, labels
 
+    def PrepareFeatureLabel_tg(self, data):
+
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        labels = data.y
+        n_nodes = x.size(0)
+        node_feat = x
+
+        labels = torch.LongTensor(len(batch_graph))
+        n_nodes = 0
+
+
+        if batch_graph[0].node_tags is not None:
+            node_tag_flag = True
+            concat_tag = []
+        else:
+            node_tag_flag = False
+
+        if batch_graph[0].node_features is not None:
+            node_feat_flag = True
+            concat_feat = []
+        else:
+            node_feat_flag = False
+
+        for i in range(len(batch_graph)):
+            labels[i] = batch_graph[i].label
+            n_nodes += batch_graph[i].num_nodes
+            if node_tag_flag:
+                concat_tag += batch_graph[i].node_tags
+            if node_feat_flag:
+                tmp = torch.from_numpy(
+                    batch_graph[i].node_features).type('torch.FloatTensor')
+                concat_feat.append(tmp)
+
+        if node_tag_flag:
+            concat_tag = torch.LongTensor(concat_tag).view(-1, 1)
+            node_tag = torch.zeros(n_nodes, cmd_args.feat_dim)
+            node_tag.scatter_(1, concat_tag, 1)
+
+        if node_feat_flag:
+            node_feat = torch.cat(concat_feat, 0)
+
+        if node_feat_flag and node_tag_flag:
+            # concatenate one-hot embedding of node tags (node labels)
+            # with continuous node features
+            node_feat = torch.cat([node_tag.type_as(node_feat), node_feat], 1)
+        elif node_feat_flag is False and node_tag_flag:
+            node_feat = node_tag
+        elif node_feat_flag and node_tag_flag is False:
+            pass
+        else:
+            node_feat = torch.ones(n_nodes, 1)
+
+        if cmd_args.mode == 'gpu':
+            node_feat = node_feat.cuda()
+            labels = labels.cuda()
+
+        return node_feat, labels
+
     def forward(self, batch_graph):
         node_feat, labels = self.PrepareFeatureLabel(batch_graph)
+
         embed = self.s2v(batch_graph, node_feat, None)
 
         return self.mlp(embed, labels)
--- network_tg.py
+++ network_tg.py
@@ -12,6 +12,7 @@ from s2v_lib import S2VLIB # noqa
 from pytorch_util import weights_init, gnn_spmm # noqa
 
 from torch_geometric.nn.models import GraphUNet
+from torch_geometric.nn import global_sort_pool
 
 class GUNet(nn.Module):
     def __init__(self, output_dim, num_node_feats, num_edge_feats,
@@ -47,15 +48,50 @@ class GUNet(nn.Module):
             self.out_params = nn.Linear(self.dense_dim, output_dim)
         # ks = [4000, 3000, 2000, 1000]
         ks = [0.9, 0.7, 0.6, 0.5]
-        self.gUnet = ops.GraphUnet(ks, num_node_feats, 97).cuda()
-        # self.gUnet = GraphUNet(num_node_feats, hidden_channels=48, out_channels=97, depth=len(ks), pool_ratios=ks).cuda()
+        # self.gUnet = ops.GraphUnet(ks, num_node_feats, 97).cuda()
+        self.gUnet = GraphUNet(num_node_feats, hidden_channels=48, out_channels=97, depth=len(ks), pool_ratios=ks).cuda()
 
         weights_init(self)
+
+    @staticmethod
+    def PrepareMeanField(data):
+        total_num_nodes, total_num_edges = data.x.size(0), data.edge_index.size(1)
+
+        n2n_idxes = torch.LongTensor(2, total_num_edges * 2)
+        n2n_vals = torch.FloatTensor(total_num_edges * 2)
+
+        e2n_idxes = torch.LongTensor(2, total_num_edges * 2)
+        e2n_vals = torch.FloatTensor(total_num_edges * 2)
+
+        subg_idxes = torch.LongTensor(2, total_num_nodes)
+        subg_vals = torch.FloatTensor(total_num_nodes)
+
+        idx_list = (ctypes.c_void_p * 3)()
+        idx_list[0] = n2n_idxes.numpy().ctypes.data
+        idx_list[1] = e2n_idxes.numpy().ctypes.data
+        idx_list[2] = subg_idxes.numpy().ctypes.data
+
+        val_list = (ctypes.c_void_p * 3)()
+        val_list[0] = n2n_vals.numpy().ctypes.data
+        val_list[1] = e2n_vals.numpy().ctypes.data
+        val_list[2] = subg_vals.numpy().ctypes.data
+
+        self.lib.PrepareMeanField(self.batch_graph_handle,
+                                  ctypes.cast(idx_list, ctypes.c_void_p),
+                                  ctypes.cast(val_list, ctypes.c_void_p))
+
+        n2n_sp = torch.sparse.FloatTensor(n2n_idxes, n2n_vals, torch.Size([total_num_nodes, total_num_nodes]))
+        e2n_sp = torch.sparse.FloatTensor(e2n_idxes, e2n_vals, torch.Size([total_num_nodes, total_num_edges * 2]))
+        subg_sp = torch.sparse.FloatTensor(subg_idxes, subg_vals, torch.Size([len(graph_list), total_num_nodes]))
+
+
+        return n2n_sp, e2n_sp, subg_sp
 
     def forward(self, graph_list, node_feat, edge_feat):
         graph_sizes = [graph_list[i].num_nodes for i in range(len(graph_list))]
         node_degs = [torch.Tensor(graph_list[i].degs) + 1
                      for i in range(len(graph_list))]
+
         node_degs = torch.cat(node_degs).unsqueeze(1)
 
         n2n_sp, e2n_sp, subg_sp = S2VLIB.PrepareMeanField(graph_list)
@@ -78,6 +114,64 @@ class GUNet(nn.Module):
             node_degs)
         return h
 
+    def sortpooling_embedding_tg(self, data):
+        ''' if exists edge feature, concatenate to node feature vector '''
+        node_feat, edge_index, batch = data.x, data.edge_index, data.batch
+        if data.edge_attr is not None:
+            input_edge_linear = self.w_e2l(data.edge_attr)
+            e2npool_input = gnn_spmm(e2n_sp, input_edge_linear)
+            node_feat = torch.cat([node_feat, e2npool_input], 1)
+
+        cur_message_layer = self.gUnet(x=node_feat, edge_index=edge_index, batch=batch)
+        batch_sortpooling_graphs = global_sort_pool(cur_message_layer, batch, self.k)
+
+
+        ''' sortpooling layer '''
+        sort_channel = cur_message_layer[:, -1]
+        batch_sortpooling_graphs = torch.zeros(
+            len(graph_sizes), self.k, self.total_latent_dim)
+        if isinstance(node_feat.data, torch.cuda.FloatTensor):
+            batch_sortpooling_graphs = batch_sortpooling_graphs.cuda()
+
+        batch_sortpooling_graphs = Variable(batch_sortpooling_graphs)
+
+        accum_count = 0
+        for i in range(subg_sp.size()[0]):
+            to_sort = sort_channel[accum_count: accum_count + graph_sizes[i]]
+            k = self.k if self.k <= graph_sizes[i] else graph_sizes[i]
+            _, topk_indices = to_sort.topk(k)
+            topk_indices += accum_count
+            sortpooling_graph = cur_message_layer.index_select(0, topk_indices)
+            if k < self.k:
+                to_pad = torch.zeros(self.k - k, self.total_latent_dim)
+                if isinstance(node_feat.data, torch.cuda.FloatTensor):
+                    to_pad = to_pad.cuda()
+
+                to_pad = Variable(to_pad)
+                sortpooling_graph = torch.cat((sortpooling_graph, to_pad), 0)
+            batch_sortpooling_graphs[i] = sortpooling_graph
+            accum_count += graph_sizes[i]
+
+        ''' traditional 1d convlution and dense layers '''
+        to_conv1d = batch_sortpooling_graphs.view(
+            (-1, 1, self.k * self.total_latent_dim))
+        conv1d_res = self.conv1d_params1(to_conv1d)
+        conv1d_res = F.relu(conv1d_res)
+        conv1d_res = self.maxpool1d(conv1d_res)
+        conv1d_res = self.conv1d_params2(conv1d_res)
+        conv1d_res = F.relu(conv1d_res)
+
+        to_dense = conv1d_res.view(len(graph_sizes), -1)
+
+        if self.output_dim > 0:
+            out_linear = self.out_params(to_dense)
+            reluact_fp = F.relu(out_linear)
+        else:
+            reluact_fp = to_dense
+
+        return F.relu(reluact_fp)
+
+
     def sortpooling_embedding(self, node_feat, edge_feat, n2n_sp, e2n_sp,
                               subg_sp, graph_sizes, node_degs):
         ''' if exists edge feature, concatenate to node feature vector '''
@@ -89,10 +183,13 @@ class GUNet(nn.Module):
         ''' graph convolution layers '''
         A = ops.normalize_adj(n2n_sp)
 
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
         ver = 2
 
         if ver == 2:
-            cur_message_layer = self.gUnet(A, node_feat)
+            # cur_message_layer = self.gUnet(A, node_feat)
+            cur_message_layer = self.gUnet(x=x, edge_index=edge_index, batch=batch)
         else:
             lv = 0
             cur_message_layer = node_feat
@@ -135,7 +232,6 @@ class GUNet(nn.Module):
         ''' traditional 1d convlution and dense layers '''
         to_conv1d = batch_sortpooling_graphs.view(
             (-1, 1, self.k * self.total_latent_dim))
-
         conv1d_res = self.conv1d_params1(to_conv1d)
         conv1d_res = F.relu(conv1d_res)
         conv1d_res = self.maxpool1d(conv1d_res)
